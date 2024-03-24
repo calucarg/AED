97813015ea1c9a6c5c1861256b38203553d71c59
calucarg <lucianocalobiniuba@gmail.com>
2024-03-24 00:24:09 -0300

    test hechpos

--- laboratorios/talleres/taller-2/solucion/src/test/java/aed/CoberturaTests.java
+++ laboratorios/talleres/taller-2/solucion/src/test/java/aed/CoberturaTests.java
@@ -11,16 +11,29 @@ class CoberturaTests {
 
     @Test
     void testFizzBuzz() {
-        assertEquals("A","B");
+        assertEquals("FizzBuzz" ,cobertura.fizzBuzz(0));
+        assertEquals("FizzBuzz" ,cobertura.fizzBuzz(15));
+        assertEquals("Fizz", cobertura.fizzBuzz(9));
+        assertEquals("Buzz", cobertura.fizzBuzz(20));
+        assertEquals("17", cobertura.fizzBuzz(17));
     }
 
     @Test
     void testNumeroCombinatorio() {
-        assertTrue(false);
+        assertEquals(1, cobertura.numeroCombinatorio(0, 0));
+        assertEquals(1, cobertura.numeroCombinatorio(10, 10));
+        assertEquals(1, cobertura.numeroCombinatorio(10, 0));
+        assertEquals(10, cobertura.numeroCombinatorio(10, 1));
+        assertEquals(252, cobertura.numeroCombinatorio(10, 5));
     }
 
     @Test
     void testRepeticionesConsecutivas() {
-        assertTrue(false);
+        assertEquals(0, cobertura.repeticionesConsecutivas(new int[]{}));
+        assertEquals(6, cobertura.repeticionesConsecutivas(new int[]{5,5,5,5,5,5}));
+        assertEquals(0, cobertura.repeticionesConsecutivas(new int[]{5}));
+        assertEquals(1, cobertura.repeticionesConsecutivas(new int[]{1,2,3,4,5}));
+        assertEquals(5, cobertura.repeticionesConsecutivas(new int[]{1,1,1,1,1,2,2,2,2,2,3,3,3,5,5,5}));
+        assertEquals(2, cobertura.repeticionesConsecutivas(new int[]{1,2,3,4,4,5,6,7}));
     }
 }
